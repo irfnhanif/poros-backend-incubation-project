4cfa21e280e96243f8323a0aeeba4a2997c4e482
chocobarrel <14.irfanh@gmail.com>
2023-03-24 11:35:18 +0700

    updated server.js

--- app/src/server.js
+++ app/src/server.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+const db = require("./models/index");
+
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("Synced database.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync database: " + err.message);
+  });
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use("/api", require("./routes/index"));
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
