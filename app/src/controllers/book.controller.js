e75946867c6d7675ee4fbf6f86fa617499b6d073
chocobarrel <14.irfanh@gmail.com>
2023-03-24 16:50:55 +0700

    added two methods in book.controller

--- app/src/controllers/book.controller.js
+++ app/src/controllers/book.controller.js
@@ -1,11 +1,78 @@
-const { Book } = require("../models/index");
+const Book = require("../models/book.model");
 
-exports.create = (req, res) => {
-    
+exports.create = async (req, res) => {
+  if (!req.body || !req.body.title) {
+    res.status(400).json({
+      success: false,
+      message: "Judul tidak boleh kosong!",
+    });
+  }
+
+  const book = {
+    title: req.body.title,
+    genre: req.body.genre,
+    author: req.body.author,
+    publisher: req.body.publisher,
+    yearPublished: Number(req.body.yearPublished),
+    totalPage: Number(req.body.totalPage),
+    currentPage: Number(req.body.currentPage),
+    bookStatus:
+      Number(req.body.currentPage) === Number(req.body.totalPage)
+        ? "Selesai dibaca"
+        : "Sedang dibaca",
+  };
+
+  try {
+    const newBook = await Book.create(book);
+    res.status(201).json({
+      success: true,
+      message: "Buku berhasil dimasukkan!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 exports.findAll = (req, res) => {};
 
-exports.update = (req, res) => {};
+exports.update = async (req, res) => {
+  const bookId = Number(req.params.id);
+  const book = await Book.findByPk(bookId);
+
+  if (!book) {
+    res.status(404).json({
+      success: false,
+      message: "Buku tidak ditemukan!",
+    });
+  }
+
+  try {
+    const keys = Object.keys(req.body);
+    keys.forEach((key) => {
+      if (
+        key === "yearPublished" ||
+        key === "totalPage" ||
+        key === "currentPage"
+      ) {
+        book[key] = Number(req.body[key]);
+      } else if (req.body[key]) {
+        book[key] = req.body[key];
+      }
+    });
+    await book.save();
+    res.status(200).json({
+      success: true,
+      message: "Buku berhasil diperbarui!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 exports.delete = (req, res) => {};
